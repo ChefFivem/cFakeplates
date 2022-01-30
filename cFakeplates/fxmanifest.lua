82f42fcee2a15792e6eabe6b97c4fb533d28e142
Chef <67842030+ChefFivem@users.noreply.github.com>
2022-01-30 12:31:53 +0100

    Add files via upload

---
+++ cFakeplates/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'adamant'
+games { 'gta5' }
+lua54 'yes'
+
+author "chef#6502"
+description "Changement de plaque"
+
+client_scripts {
+    'client/main.lua',
+    'config.lua'
+}
+
+server_scripts {
+    'server/main.lua',
+    'config.lua'
+}
+dependency '/assetpacks'
